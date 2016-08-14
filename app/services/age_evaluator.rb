5b5140ebea5770f92c4393dcb860cf7800b82da7
Sean Sellek <me@seansellek.com>
2016-08-14 09:31:00 -0400

    Fix age evaluator

--- app/services/age_evaluator.rb
+++ app/services/age_evaluator.rb
@@ -2,6 +2,7 @@ class AgeEvaluator
   def self.call(condition, appointment)
     dob = appointment["appointment"]["patient"]["date_of_birth"]
     age = (Time.now - Date.parse(dob).to_time) / 60 / 60 / 24 / 365
+    condition["operator"] = "==" if condition["operator"] == "="
     age.send(condition["operator"], condition["value"].to_i)
   end
 end
